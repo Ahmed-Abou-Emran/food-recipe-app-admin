eadb9da2cf0cdfb2db172b3aed8f03f714043179
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-11-30 15:50:46 +0200

    adding assets

---
+++ src/assets/index.jsx
@@ -0,0 +1,16 @@
+import SideBarLogo from "./sidebar-logo-.png";
+import HeaderBg from "./header-bg.svg";
+import HeaderHome from "./header-home.svg";
+import HeaderRecipes from "./header-recipes.svg";
+import NotFound from "./notFound.svg";
+import NoData from "./NoData.svg";
+import PersonalImage from "./Personal-Image.jpg";
+export {
+  PersonalImage,
+  SideBarLogo,
+  HeaderBg,
+  HeaderHome,
+  HeaderRecipes,
+  NotFound,
+  NoData,
+};
