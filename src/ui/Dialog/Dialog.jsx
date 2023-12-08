02973e272a45a42c27ad6a544997b1347d10a56b
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-12-08 19:55:32 +0200

    adding accessible dialog component

---
+++ src/ui/Dialog/Dialog.jsx
@@ -0,0 +1,247 @@
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { ImCross as Cross } from "react-icons/im";
+import {
+  FaRegTrashAlt as Trash,
+  FaEye as Eye,
+  FaRegEdit as Edit,
+} from "react-icons/fa";
+import "./Dialog.css";
+import { NoData } from "../../assets";
+import styled from "styled-components";
+const Root = Dialog.Root;
+
+// will contain the button that will open the dialog
+const Trigger = Dialog.Trigger;
+
+const Overlay = Dialog.Overlay;
+const Portal = Dialog.Portal;
+
+const Content = Dialog.Content;
+const Title = Dialog.Title;
+const Description = Dialog.Description;
+
+// will close the dialog
+const Close = Dialog.Close;
+
+const DialogDemo = () => (
+  <Root>
+    <Trigger asChild>
+      <button className="Button violet">Edit profile</button>
+    </Trigger>
+    <Portal>
+      <Overlay className="DialogOverlay" />
+      <Content className="DialogContent">
+        <Title className="DialogTitle">Edit profile</Title>
+        <Description className="DialogDescription">
+          Make changes to your profile here. Click save when you're done.
+        </Description>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="name">
+            Name
+          </label>
+          <input className="Input" id="name" defaultValue="Pedro Duarte" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="username">
+            Username
+          </label>
+          <input className="Input" id="username" defaultValue="@peduarte" />
+        </fieldset>
+        <div
+          style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}
+        >
+          <Close asChild>
+            <button className="Button green">Save changes</button>
+          </Close>
+        </div>
+        <Close asChild>
+          <button className="IconButton" aria-label="Close">
+            <Cross />
+          </button>
+        </Close>
+      </Content>
+    </Portal>
+  </Root>
+);
+
+export const DeleteDialog = ({ itemName, onDelete, open, onOpenChange }) => {
+  return (
+    <Wrapper open={open} onOpenChange={onOpenChange}>
+      <ActionTrigger>
+        <Trash />
+      </ActionTrigger>
+      <Portal>
+        <Overlay className="DialogOverlay" />
+        <ContentWrapper className="DialogContent">
+          <ImageWrapper>
+            <img src={NoData} />
+          </ImageWrapper>
+          <h3>Delete This {itemName ? itemName : "Item"}?</h3>
+          <p>
+            are you sure you want to delete this {itemName ? itemName : "Item"}?
+            if you are sure just click on delete it
+          </p>
+
+          {/* <Close asChild> */}
+          <OutlineButton onClick={onDelete}>
+            Delete This {itemName ? itemName : "Item"}
+          </OutlineButton>
+          {/* </Close> */}
+          <Close asChild>
+            <CrossButton aria-label="Close">
+              <Cross />
+            </CrossButton>
+          </Close>
+        </ContentWrapper>
+      </Portal>
+    </Wrapper>
+  );
+};
+
+export const FormDialog = ({ open, onOpenChange, children }) => {
+  return (
+    <Wrapper open={open} onOpenChange={onOpenChange}>
+      <Portal>
+        <Overlay className="DialogOverlay" />
+        <FormContentWrapper className="DialogContent">
+          {children}
+
+          <Close asChild>
+            <CrossButton aria-label="Close">
+              <Cross />
+            </CrossButton>
+          </Close>
+        </FormContentWrapper>
+      </Portal>
+    </Wrapper>
+  );
+};
+export const ViewDialog = ({ children }) => {
+  return (
+    <Wrapper>
+      <ActionTrigger>
+        <Eye />
+      </ActionTrigger>
+      <Portal>
+        <Overlay className="DialogOverlay" />
+        <ContentWrapper className="DialogContent">
+          {children}
+          <Close asChild>
+            <CrossButton aria-label="Close">
+              <Cross />
+            </CrossButton>
+          </Close>
+        </ContentWrapper>
+      </Portal>
+    </Wrapper>
+  );
+};
+const Wrapper = styled(Root)`
+  position: relative;
+`;
+
+const ContentWrapper = styled(Content)`
+  padding-inline: var(--spacing-80);
+  padding-block: var(--spacing-100);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  h3 {
+    color: var(--grey-700);
+    font-family: Inter;
+    font-size: 1.25rem;
+    font-weight: 700;
+  }
+  p {
+    color: var(--grey-400);
+    text-align: center;
+    font-size: 1rem;
+    font-weight: 400;
+    margin-block-end: var(--spacing-60);
+  }
+
+  button {
+    align-self: flex-end;
+  }
+`;
+
+const FormContentWrapper = styled(ContentWrapper)`
+  align-items: stretch;
+`;
+const ActionTrigger = styled(Trigger)`
+  color: var(--grey-600);
+  transition: all 300ms ease-in-out;
+  &:focus,
+  &:hover {
+    color: var(--grey-800);
+    transform: scale(2);
+    outline: none;
+  }
+  svg {
+    font-size: 1.5rem;
+  }
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+const ImageWrapper = styled.div`
+  height: 15rem;
+  font-size: 5rem;
+  color: var(--grey-400);
+  img {
+    /* width: 100%; */
+    height: 100%;
+  }
+`;
+
+const OutlineButton = styled.button`
+  color: var(--red-600);
+  border: 1px solid var(--red-600);
+  border-radius: 0.5rem;
+  padding-inline: var(--spacing-20);
+  padding-block: var(--spacing-30);
+  transition: all 300ms ease-in-out;
+  cursor: pointer;
+  &:hover {
+    color: var(--grey-100);
+    border: 1px solid var(--grey-100);
+    background-color: var(--red-600);
+  }
+
+  &:focus {
+    outline: 2px solid red;
+    outline-offset: 4px;
+  }
+`;
+const IconButton = styled.button`
+  transition: all 300ms ease-in-out;
+  border-radius: 50%;
+  padding: 6px;
+  &:focus,
+  &:hover {
+    /* color: var(--red-500); */
+    transform: scale(1.2);
+    outline: none;
+  }
+  svg {
+    font-size: 1.2rem;
+  }
+  background: none;
+  cursor: pointer;
+`;
+const CrossButton = styled(IconButton)`
+  position: absolute;
+  top: var(--spacing-80);
+  right: var(--spacing-80);
+  color: var(--red-600);
+  border: 2px solid var(--red-600);
+  &:focus,
+  &:hover {
+    color: var(--red-500);
+  }
+`;
+
+export default DialogDemo;
