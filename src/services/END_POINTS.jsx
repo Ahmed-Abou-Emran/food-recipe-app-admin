d6c6939df3314fb76d05725ee1a74adc0cc9465a
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-12-12 21:48:52 +0200

    adding end points

---
+++ src/services/END_POINTS.jsx
@@ -0,0 +1,16 @@
+/************************************Base************************************/
+export const BASE_URL = "https://upskilling-egypt.com:443/api/v1";
+
+/************************************Categories************************************/
+
+export const categoriesURL = `${BASE_URL}/Category`;
+
+/************************************Users************************************/
+
+export const usersURL = `${BASE_URL}/Users`;
+
+/************************************Recipes************************************/
+export const recipesURL = `${BASE_URL}/Recipe`;
+
+/************************************Tags************************************/
+export const tagsURL = `${BASE_URL}/tag`;
