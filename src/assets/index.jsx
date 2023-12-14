3194029cc466295fbb5b303a0de5fd400c4bfcc7
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-12-14 14:03:16 +0200

    updating assets

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -4,13 +4,4 @@ import HeaderHome from "./header-home.svg";
 import HeaderRecipes from "./header-recipes.svg";
 import NotFound from "./notFound.svg";
 import NoData from "./NoData.svg";
-import PersonalImage from "./Personal-Image.jpg";
-export {
-  PersonalImage,
-  SideBarLogo,
-  HeaderBg,
-  HeaderHome,
-  HeaderRecipes,
-  NotFound,
-  NoData,
-};
+export { SideBarLogo, HeaderBg, HeaderHome, HeaderRecipes, NotFound, NoData };
